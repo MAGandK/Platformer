8ad224d241ed8910e7729c1c9ef45c50afea1e13
AnnaMigitenko <migitenko@gmail.com>
2024-06-03 22:28:28 +0300

    add button exit

--- Platformer/Assets/Platformer/Scrips/UIController.cs
+++ Platformer/Assets/Platformer/Scrips/UIController.cs
@@ -1,15 +1,19 @@
-using System;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
+using DG.Tweening;
 
 public class UIController : MonoBehaviour
 {
     public static UIController Instance;
     
-    [SerializeField] private Transform _canvasParrentTransform;
-    [SerializeField] private GameObject _uiPopup;
-    private bool _isPause;
-
+    [SerializeField] private Transform _canvasParentTransform;
+    [SerializeField] private GameObject _uiPopupPrefab;
+    [SerializeField] private TMP_Text _gameOverText;
+    
+    private GameObject _currentPopup;
+    
+    
     private void Awake()
     {
         if (Instance == null)
@@ -19,13 +23,24 @@ public class UIController : MonoBehaviour
     }
     public void ShowPopup()
     {
-        GameObject pupap = Instantiate(_uiPopup);
-        
-        pupap.transform.SetParent(_canvasParrentTransform,false);
+        _currentPopup = Instantiate(_uiPopupPrefab);
+            
+        _currentPopup.transform.SetParent(_canvasParentTransform, false);
 
-        RectTransform rectTransform = pupap.GetComponent<RectTransform>();
-        
+        RectTransform rectTransform = _currentPopup.GetComponent<RectTransform>();
         rectTransform.anchoredPosition = Vector2.zero;
-        rectTransform.sizeDelta = new Vector2(500, 400);
+
+        DOTween.Sequence()
+            .Append(_currentPopup.transform.DOScale(1f, 1f))
+            .Append(_currentPopup.transform.DOScale(3f, 1f))
+            .SetUpdate(true);
+    }
+
+    public void ClosePopup()
+    {
+        if (_currentPopup != null)
+        {
+            _currentPopup.SetActive(false);
+        }
     }
 }
